f4403a2439635c2b74076944c72ef09a21a5ecfb
oBoywid2024 - Home <odinboywid@gmail.com>
2025-04-09 18:32:13 -0500

    Refined Emerald progress

---
+++ src/main/java/net/profdoge/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.profdoge.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.profdoge.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+    public static final Item EMERALD_REFINED = registerItem("emerald_refined", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(EMERALD_REFINED);
+        });
+    }
+}
